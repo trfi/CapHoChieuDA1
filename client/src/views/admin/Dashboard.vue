18b2553cdd2bb05fe851695669b9c1030250ec21
tr-fi <gnourtf@gmail.com>
2020-04-09 17:01:22 +0700

    update

--- client/src/views/admin/Dashboard.vue
+++ client/src/views/admin/Dashboard.vue
@@ -6,17 +6,21 @@
       
       <v-card flat v-for="passport in passports" :key="passport.title">
         <v-layout row wrap :class="`pa-3 passport ${passport.tinhtrang}`">
-          <v-flex xs12 md6>
+          <v-flex xs12 md3>
             <div class="caption grey--text">Họ tên</div>
             <div>{{ passport.hoten }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Giới tính</div>
+            <div>{{ passport.gioitinh }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">CMND</div>
             <div>{{ passport.cmnd }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
-            <div class="caption grey--text">Giới tính</div>
-            <div>{{ passport.gioitinh }}</div>
+            <div class="caption grey--text">Địa chỉ thường trú</div>
+            <div>{{ passport.dcthuongtru }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
             <div class="right">
@@ -28,8 +32,8 @@
       </v-card>
 
     </v-container>
-   
   </div>
+  
 </template>
 
 <script>
@@ -38,7 +42,7 @@ import PassportServices from '@/services/PassportServices'
 export default {
   data () {
     return {
-      passports: {},
+      passports: {}
     }
   },
   async mounted () {
